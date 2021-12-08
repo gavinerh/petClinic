8dc47d5005924b41633df0f20ba28ca2a2729be0
gavin erh <gavinerh@gmail.com>
2021-12-08 18:15:16 +0800

    added base entity

---
+++ src/main/java/com/example/petClinic/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.example.petClinic.model;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class BaseEntity implements Serializable{
+
+	private Integer id;
+	
+}
